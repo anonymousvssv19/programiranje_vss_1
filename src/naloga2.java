970d4b964983dffa17cc8ec52113ae10962d471f
annonymousvss19 <nejc.severmihelic@scv.si>
2024-01-16 09:43:52 +0100

    vaja 2

---
+++ src/naloga2.java
@@ -0,0 +1,10 @@
+public class naloga2 {
+    public static void main(String[] args) {
+
+        System.out.println("Ime: Nejc");
+
+        System.out.println("Priimek: Sever Mihelič");
+
+        System.out.println("Elektrnoski_naslov: nejc.severmihelic@scv.si");
+    }
+}
