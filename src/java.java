fb98089902e1602b82bbf26d40ef5a1f54ece639
annonymousvss19 <nejc.severmihelic@scv.si>
2024-01-16 09:18:24 +0100

    osnove

--- src/java.java
+++ src/java.java
@@ -1,5 +1,8 @@
 public class java {
     public static void main(String[] args) {
         System.out.print("Nejc Sever Mihelič");
+        System.out.print("\n\n");
+        System.out.printf("Pri primer");
+        System.out.printf("\nIme: %s priimek: %s", "Nejc", "Sever");
     }
 }
