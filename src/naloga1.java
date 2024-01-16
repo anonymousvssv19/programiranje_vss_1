a8387783158a4b301fffad6f83080f64249af29c
annonymousvss19 <nejc.severmihelic@scv.si>
2024-01-16 09:32:14 +0100

    vaja 1 izpis 4ih besed

---
+++ src/naloga1.java
@@ -0,0 +1,14 @@
+public class naloga1 {
+    public static void main(String[] args) {
+
+        String beseda1 = "beseda1";
+        String beseda2 = "beseda2";
+        String beseda3 = "beseda3";
+        String beseda4 = "beseda4";
+
+        System.out.println(beseda1);
+        System.out.println(beseda2);
+        System.out.println(beseda3);
+        System.out.println(beseda4);
+    }
+}
